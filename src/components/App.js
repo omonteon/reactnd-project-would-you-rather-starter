3e580657d496010165119b731d163764141faf97
Omar Monteon <monteon.omar@gmail.com>
2021-04-01 21:12:20 -0600

    feat: Connect App and get initial data on mount.

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,29 @@
+import { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { handleInitialData } from '../actions/shared';
 import Login from './Login';
 import Polls from './Polls';
 
-function App() {
-  return (
-    <Router>
-    <div className="App">
-      {/* NAV */}
-      <Route path="/" exact component={Polls} />
-      <Route path="/login" component={Login} />
-      {/* /poll/:id */}
-      {/* /leaderboard */}  
-      {/* /new */}
-    </div>
-    </Router>
-  );
+class App extends Component {
+  componentDidMount() {
+    const { dispatch } = this.props;
+    dispatch(handleInitialData());
+  }
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          {/* NAV */}
+          <Route path="/" exact component={Polls} />
+          <Route path="/login" component={Login} />
+          {/* /poll/:id */}
+          {/* /leaderboard */}
+          {/* /new */}
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+export default connect(state => state)(App)
