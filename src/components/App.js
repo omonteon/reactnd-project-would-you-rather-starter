132b4c819d61c39b5932391f17cbcfc6579b8693
Omar Monteon <monteon.omar@gmail.com>
2021-04-01 19:48:35 -0600

    feat: Add root route.

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Login from './Login';
+import Polls from './Polls';
 
 function App() {
   return (
     <Router>
     <div className="App">
       {/* NAV */}
-      {/* <Route path="/" exact component={Polls} /> */}
+      <Route path="/" exact component={Polls} />
       <Route path="/login" component={Login} />
       {/* /poll/:id */}
       {/* /leaderboard */}  
---
+++ src/components/Polls.js
@@ -0,0 +1,5 @@
+function Polls() {
+  return (<>Polls</>);
+}
+
+export default Polls;
