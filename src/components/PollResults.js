2db198da8650481e54b1e57d5a862dfb61a29a89
Omar Monteon <monteon.omar@gmail.com>
2021-04-02 18:46:01 -0600

    feat: Create PollResults component.

---
+++ src/components/PollResults.js
@@ -0,0 +1,22 @@
+import PollOption from './PollOption';
+
+// CODE REVIEW QUESTION: 
+// Is it ok to pass this info as props ? 
+// or should this component be connected to the store and get the info from there ?
+function PollResults({ authedUserId, question }) {
+  const { optionOne, optionTwo } = question;
+  const totalVotes = optionOne.votes.length + optionTwo.votes.length;
+  return <div className="poll-results">
+    <h3>Results:</h3>
+    <PollOption
+      option={optionOne}
+      totalVotes={totalVotes}
+      votedOption={optionOne.votes.find(vote => vote === authedUserId)} />
+    <PollOption
+      option={optionTwo}
+      totalVotes={totalVotes}
+      votedOption={optionTwo.votes.find(vote => vote === authedUserId)} />
+  </div>;
+}
+
+export default PollResults;
