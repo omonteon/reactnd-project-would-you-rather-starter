c24f9e9186d333e8d97c0adec6993a2ec6110e29
Omar Monteon <monteon.omar@gmail.com>
2021-04-02 18:00:32 -0600

    feat: Implement async action creator.

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,16 +1,33 @@
 
+import { saveQuestionAnswer } from '../utils/api';
+
 export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
 export const ANSWER_QUESTION = 'ANSWER_QUESTION'
 export const ADD_QUESTION = 'ADD_QUESTION'
 
-export function answerQuestion({ id, authedUser, selectedOption }) {
+function answerQuestion({ authedUser, qid, answer }) {
   return {
     type: ANSWER_QUESTION,
-    id,
     authedUser,
-    selectedOption
+    qid,
+    answer
   }
 }
+
+export function handleSaveQuestionAnswer(info) {
+  return (dispatch) => {
+    dispatch(answerQuestion(info))
+
+    return saveQuestionAnswer(info)
+      .catch((e) => {
+        console.warn('Error in handleSaveQuestionAnswer: ', e)
+        // TODO: Implement "undo" action
+        // dispatch(answerQuestion(info))
+        alert('The was an error saving your answer. Try again.')
+      })
+  }
+}
+
 
 export function addQuestion(question) {
   return {
