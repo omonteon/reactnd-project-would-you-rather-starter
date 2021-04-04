29919f8122fa2b1b5204605d3f1c94740ba46a6f
Omar Monteon <monteon.omar@gmail.com>
2021-04-03 19:03:05 -0600

    feat: Create Nav component.

---
+++ src/components/Nav.js
@@ -0,0 +1,25 @@
+import { NavLink } from 'react-router-dom'
+
+export default function Nav () {
+  return (
+    <nav className='nav'>
+      <ul>
+        <li>
+          <NavLink to='/' exact activeClassName='active'>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/add' activeClassName='active'>
+            New Question
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to='/leaderboard' activeClassName='active'>
+            Leader Board
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
