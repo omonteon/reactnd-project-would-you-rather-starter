12cf1d7cf3c3be223c03c8883a87759ae5754cd1
Omar Monteon <monteon.omar@gmail.com>
2021-04-04 15:13:06 -0500

    refactor: Move styles file to folder.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
-import './index.css';
+import './styles/index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import middleware from './middleware';
