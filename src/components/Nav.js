1032006bab519c9c2dacd506afc4c6d2cbccfd72
Omar Monteon <monteon.omar@gmail.com>
2021-04-04 10:32:03 -0500

    feat: Implement Logout.

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,25 +1,50 @@
-import { NavLink } from 'react-router-dom'
+import { Component } from 'react'
+import { connect } from 'react-redux';
+import { setAuthedUser } from '../actions/authedUser';
+import { NavLink, Link } from 'react-router-dom'
 
-export default function Nav () {
-  return (
-    <nav className='nav'>
-      <ul>
-        <li>
-          <NavLink to='/' exact activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/add' activeClassName='active'>
-            New Question
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/leaderboard' activeClassName='active'>
-            Leader Board
-          </NavLink>
-        </li>
-      </ul>
-    </nav>
-  )
+class Nav extends Component {
+  handleLogout = () => {
+    const { dispatch } = this.props;
+    dispatch(setAuthedUser(null))
+  }
+  render() {
+    const { authedUserName, authedUserAvatarURL } = this.props;
+    return (
+      <nav className='nav'>
+        <ul>
+          <li>
+            <NavLink to='/' exact activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/add' activeClassName='active'>
+              New Question
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to='/leaderboard' activeClassName='active'>
+              Leader Board
+            </NavLink>
+          </li>
+        </ul>
+        <div className={`auth-info ${authedUserName ? '' : 'hide'}`}>
+          Hello, {authedUserName} <img src={authedUserAvatarURL} alt="User avatar" />
+          <Link to='/login' onClick={this.handleLogout}>
+            <span>Logout</span>
+          </Link>
+        </div>
+      </nav>
+    )
+  }
 }
+
+function mapStateToProps({ users, authedUser }) {
+  return {
+    authedUserName: users[authedUser] ? users[authedUser].name : '',
+    authedUserAvatarURL: users[authedUser] ? users[authedUser].avatarURL : ''
+  }
+}
+
+export default connect(mapStateToProps)(Nav)
