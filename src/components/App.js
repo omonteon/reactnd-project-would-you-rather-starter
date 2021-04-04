558325b7f00c5db33a94507ddd244b4f2466da70
Omar Monteon <monteon.omar@gmail.com>
2021-04-04 11:04:18 -0500

    feat: Add Nav and Leaderboard routes.
    Get initial data only if logged in.

--- src/components/App.js
+++ src/components/App.js
@@ -2,27 +2,38 @@ import { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { handleInitialData } from '../actions/shared';
+import Nav from './Nav';
 import Login from './Login';
 import Home from './Home';
 import Poll from './Poll';
 import AddQuestion from './AddQuestion';
+import Leaderboard from './Leaderboard';
 import '../styles/App.css';
 
 class App extends Component {
   componentDidMount() {
-    const { dispatch } = this.props;
-    dispatch(handleInitialData());
+    const { dispatch, authedUser } = this.props;
+    if (authedUser) {
+      dispatch(handleInitialData());
+    }
+  }
+  componentDidUpdate(prevProps) {
+    const { dispatch, authedUser } = this.props;
+    if (!prevProps.authedUser && authedUser) {
+      dispatch(handleInitialData()); 
+    }
   }
   render() {
+    const { authedUserName } = this.props;
     return (
       <Router>
         <div className="App">
-          {/* NAV */}
-          <Route path="/" exact component={Home} />
-          <Route path="/login" component={Login} />
-          <Route path="/questions/:id" component={Poll} />
-          <Route path="/add" component={AddQuestion} />
-          {/* /leaderboard */}
+          <Nav authedUserName={authedUserName} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/questions/:id" component={Poll} />
+          <Route exact path="/add" component={AddQuestion} />
+          <Route exact path="/leaderboard" component={Leaderboard} />
           <div className="footer">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
         </div>
       </Router>
@@ -30,4 +41,10 @@ class App extends Component {
   }
 }
 
-export default connect()(App)
+function mapStateToProps({ authedUser }) {
+  return {
+    authedUser
+  }
+}
+
+export default connect(mapStateToProps)(App)
