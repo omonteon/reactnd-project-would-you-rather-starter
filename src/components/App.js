0e543a128d82f5e050c1b9178d653c85afe76583
Omar Monteon <monteon.omar@gmail.com>
2021-04-04 15:09:15 -0500

    feat: Update styles to have only one container.
    Adjust all pages to be the container width.

--- src/components/App.js
+++ src/components/App.js
@@ -30,20 +30,22 @@ class App extends Component {
     const { authedUser } = this.props;
     return (
       <Router>
-        <div className="App">
+        <>
           <LoadingBar />
           {authedUser ? <Nav /> : null}
-          <Switch>
-            <Route exact path="/login" component={Login} />
-            <Route exact path="/404" component={PageNotFound} />
-            <PrivateRoute exact path="/" component={Home} />
-            <PrivateRoute exact path="/questions/:id" redirectPath="/404" component={Poll} />
-            <PrivateRoute exact path="/add" component={AddQuestion} />
-            <PrivateRoute exact path="/leaderboard" component={Leaderboard} />
-            <Route render={() => <Redirect to="/404" />} />
-          </Switch>
-          <div className="footer">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-        </div>
+          <div className="container">
+            <Switch>
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/404" component={PageNotFound} />
+              <PrivateRoute exact path="/" component={Home} />
+              <PrivateRoute exact path="/questions/:id" redirectPath="/404" component={Poll} />
+              <PrivateRoute exact path="/add" component={AddQuestion} />
+              <PrivateRoute exact path="/leaderboard" component={Leaderboard} />
+              <Route render={() => <Redirect to="/404" />} />
+            </Switch>
+            <div className="footer">Icons made by <a href="https://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
+          </div>
+        </>
       </Router>
     );
   }
