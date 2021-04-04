1a60b8ff610d962a7bf558d9471b47db7e366c0a
Omar Monteon <monteon.omar@gmail.com>
2021-04-04 12:20:23 -0500

    feat: Add percentage value for poll results option

--- src/components/PollOption.js
+++ src/components/PollOption.js
@@ -1,10 +1,12 @@
 function PollOption({ option, totalVotes, votedOption }) {
+  const percentageValue = option.votes.length / totalVotes * 100;
+  const percentage = Number.isInteger(percentageValue) ? percentageValue : parseFloat(percentageValue).toFixed(1);
   return (<div className="poll-option">
     <h4>
       Would you rather {option.text}?
   </h4>
     <b>
-      {option.votes.length} out of {totalVotes} votes.
+      {option.votes.length} out of {totalVotes} votes. ({percentage}%)
   </b>
     {votedOption
       ? <span className="badge badge-success">This was your choice</span>
